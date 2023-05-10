8b4cdef108af52a0e757de69302380b56bc7f64a
Abhiram Sannidhi <abhiramsannidhi@gmail.com>
2023-05-10 10:36:55 +0530

    first coommit

---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,11 @@
+g")
+# Button(root,image=ButtonPlay,bg="#FFFFFF",bd=0,height = 60,width = 60,command = PlayMusic).place(x=215,y=487)
+
+# ButtonStop = PhotoImage(file="Stop.png")
+# Button(root,image=ButtonStop,bg="#FFFFFF",bd=0,height=60,width=60,command=mixer.music.stop).place(x=20,y=487)
+
+# Buttonvolume = PhotoImage(file="Volume.webp")
+# Button(root,image=Buttonvolume,bg="#FFFFFF",bd=0,height=60,width=60,command=mixer.music.unpause).place(x=20,y=487)
+
+# ButtonPause = PhotoImage("pause.jpg")
+# Button(root,image=ButtonPause,bg="#FFFFFF",bd=0,height=60,width=60,command=mixer.music.pause).place(x=300,y=487
